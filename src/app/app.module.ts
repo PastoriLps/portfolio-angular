2ea81ef7a8cd4baa7355f714493216ccb63b46a1
PastoriLps <rafael211295@gmail.com>
2021-04-20 19:19:28 -0300

    remove routing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CardsComponent } from './cards/cards.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ModalService } from './services/modal/modal.service';
 import { ContactComponent } from './contact/contact.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,7 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([])
   ],
   providers: [ModalService],
   bootstrap: [AppComponent]
